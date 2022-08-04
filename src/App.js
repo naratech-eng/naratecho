d45fdace7d9a9ccb5bafb176e1c99c731fdd9e83
Sanjeewa Kumara Narayana Narayana Mudiyansela <narayans@uoguelph.ca>
2022-08-04 00:38:33 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import { Link } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <nav className="text-3xl font-bold">
+        <Link to="/">Home</Link> |{" "}
+        <Link to="services">Services</Link> |{" "}
+        <Link to="portfolio">Portfolio</Link> |{" "}
+        <Link to="about">About</Link> |{" "}
+        <Link to="contact">Contact Us</Link>
+      </nav>
     </div>
   );
 }
