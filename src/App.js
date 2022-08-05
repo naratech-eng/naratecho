6dd9faa55f517ac6483ec66facb455726ed83391
Sanjeewa Kumara Narayana Narayana Mudiyansela <narayans@uoguelph.ca>
2022-08-05 01:43:43 -0400

    created pages and set up

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
-import { Link } from 'react-router-dom';
+import { Link} from 'react-router-dom';
 import './App.css';
+
+import logo from './Assets/logo.png';
 
 function App() {
   return (
     <div className="App">
-      
-      <nav className="text-3xl font-bold">
-        <Link to="/">Home</Link> |{" "}
-        <Link to="services">Services</Link> |{" "}
-        <Link to="portfolio">Portfolio</Link> |{" "}
-        <Link to="about">About</Link> |{" "}
-        <Link to="contact">Contact Us</Link>
+      <img src={logo} alt='naratecho logo' className='float-left cursor-s-resize' />
+      <nav className="flex sm:justify-center space-x-4">
+        {
+          [
+            ['Home', '/'],
+            ['Services', '/services'],
+            ['Portfolio', 'portfolio'],
+            ['About', '/about'],
+            ['Contact Us', '/contact'],
+          ].map(([title, url])=> (
+            <Link to={url} key={title} className="rounded-lg px-3 py-2 text-slate-700 font-medium hover:bg-slate-100 hover:text-slate-900">{title}</Link>
+          ))
+        }
       </nav>
+      
+      
+      
+
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,14 +3,25 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter} from 'react-router-dom';
+import { BrowserRouter, Route, Routes,  } from 'react-router-dom';
+import About from './pages/About';
+import Services from './pages/Services';
+import Portfolio from './pages/Portfolio';
+import Contact from './pages/Contact';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
-    <App />
+    
+    <Routes>
+      <Route path="/" element={<App />}> 
+        <Route path='services' element={<Services/>}></Route>
+        <Route path='portfolio' element={<Portfolio/>}></Route>
+        <Route path='about' element={<About/>}></Route>
+        <Route path='contact' element={<Contact/>}></Route>
+      </Route>
+    </Routes>
   </BrowserRouter>
-  
 );
 
 // If you want to start measuring performance in your app, pass a function
